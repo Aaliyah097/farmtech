47346f1d445d9c0a1462f0994e6eefd7c5caddc1
Aaliyah097 <name.boltz@gmail.com>
2024-02-16 21:20:01 +0300

    письмо для завершения регистрации

--- farmtech/mail_provider.py
+++ farmtech/mail_provider.py
@@ -1,2 +1,16 @@
-class EmailProvider:
-    pass
+import os
+import ssl
+import smtplib
+from email.mime.text import MIMEText
+
+
+def send_email(recipients: list[str], message: str, subject: str, from_email: str = os.environ.get("EMAIL_SENDER")):
+    message = MIMEText(message)
+    message['Subject'] = subject
+
+    with smtplib.SMTP(os.environ.get("EMAIL_HOST"), int(os.environ.get("EMAIL_PORT"))) as connection:
+        connection.sendmail(
+            from_addr=os.environ.get("EMAIL_SENDER"),
+            to_addrs=recipients,
+            msg=message.as_string()
+        )
