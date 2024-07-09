69727bf93d4df6b5b69a3b54878d22291b9722da
Aaliyah097 <name.boltz@gmail.com>
2024-07-09 18:54:52 +0300

    импорт данных из цсв файлов

--- src/users/apps.py
+++ src/users/apps.py
@@ -24,10 +24,89 @@ class UsersConfig(AppConfig):
 
         call_command("migrate", "users")
 
+        self.load_users_jobs()
+        self.load_departments()
+        self.load_jobs()
+        self.load_users_departments()
+
+    def load_users_departments(self):
+        from src.users.models import Departments, User, Jobs
+
+        file = pd.read_csv("users.csv")
+        for idx, row in file.iterrows():
+            print('users.csv', idx)
+            last_name, first_name, middle_name, job_name, fio, email, department_name = (
+                row['Фамилия'],
+                row['Имя'],
+                row['Отчетство'],
+                row['Должность'],
+                row['ФИО'],
+                row['Email'],
+                row['Подразделение'],
+            )
+
+            job, _ = Jobs.objects.get_or_create(name=job_name)
+            dep, _ = Departments.objects.get_or_create(name=department_name)
+
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                continue
+
+            user.departments.add(dep)
+
+            user.save()
+
+    def load_jobs(self):
+        from src.users.models import Jobs
+
+        file = pd.read_csv("jobs.csv")
+        for idx, row in file.iterrows():
+            print("jobs.csv", idx)
+            name = row['Название']
+
+            try:
+                job = Jobs.objects.get(name=name)
+            except Jobs.DoesNotExist:
+                job = Jobs(name=name)
+                job.save()
+
+    def load_departments(self):
+        from src.users.models import Departments, User
+
+        file = pd.read_csv("departments.csv")
+        for idx, row in file.iterrows():
+            print("departments.csv", idx)
+            name, _, manager_email = (
+                row['Название'],
+                row['Начальник ФИО'],
+                row['Начальник почта']
+            )
+            try:
+                dep = Departments.objects.get(name=name)
+            except Departments.DoesNotExist:
+                dep = None
+
+            try:
+                manager = User.objects.get(email=manager_email)
+            except User.DoesNotExist:
+                manager = None
+
+            if dep:
+                dep.manager = manager
+            else:
+                dep = Departments(
+                    name=name,
+                    manager=manager
+                )
+            dep.save()
+
+    def load_users_jobs(self):
         from src.users.models import Jobs, User
 
         file = pd.read_csv("users_jobs.csv")
         for idx, row in file.iterrows():
+            print('users_jobs.csv', idx)
             job, fio, organization, email = (
                 row["Должность"],
                 row["ФИО"],
