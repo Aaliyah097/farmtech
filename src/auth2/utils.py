940882d3319c11859de381b64cdf7f134c548a64
Aaliyah097 <name.boltz@gmail.com>
2024-09-19 22:11:03 +0300

    права админа

--- src/auth2/utils.py
+++ src/auth2/utils.py
@@ -1,4 +1,6 @@
 def has_level_permissions(request, demand_level) -> bool:
+    if request.user.is_superuser:
+        return True
     department, level = request.user.main_department
     if level > demand_level:
         return False
