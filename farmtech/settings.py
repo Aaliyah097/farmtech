72d3e1abcd155d9056ad1b8c1ed4ff2dd6336671
Владислав Больц <vbolts@kamaz.digital>
2024-12-09 15:22:35 +0300

    allowed hosts

--- farmtech/settings.py
+++ farmtech/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "10.0.1.11"]
+ALLOWED_HOSTS = ["127.0.0.1", "0.0.0.0", "10.0.1.11", 'vks.pharmtec.ru']
 
 
 # Application definition
